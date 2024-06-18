28da62ceea85a3a88b04ccc4978313cb20fc4116
ahaduzzaman5503 <ahaduzzaman5503@gmail.com>
2024-06-19 04:15:34 +0600

    some changes

--- cypress/e2e/cssselector.cy.js
+++ cypress/e2e/cssselector.cy.js
@@ -2,34 +2,15 @@ describe("FormAutomation", () => {
   it("formautomation", () => {
     cy.fixture("example.json").then((data) => {
       data.forEach((userdata) => {
-        cy.visit(
-          "https://docs.google.com/forms/d/e/1FAIpQLScvqstDokohgT3643DeMx_Rlg71rIKLkS8sZ7jFN2sTWY-QbQ/viewform"
-        );
-
-        // Generate a random index to select one of the four radio buttons
-        const radioButtons = ["#i8", "#i5", "#i11", "#i14"];
-        const randomIndex = Math.floor(Math.random() * radioButtons.length);
-        const selectedRadioButton = radioButtons[randomIndex];
-
-        // Click the randomly selected radio button
-        cy.get(selectedRadioButton)
-          .click()
-          .should("have.attr", "aria-checked", "true");
-
-        // Fill in the form fields
+        cy.visit("https://docs.google.com/forms/d/e/1FAIpQLSeO8XBxpKS6gNOw9byfjcwC8A18xB9USgnPkqHqkDWvVgZKcA/viewform");
+        cy.get("#i11").click().should("have.attr", "aria-checked", "true");
         cy.get("input.whsOnd").eq(0).type(userdata.name);
         cy.get("input.whsOnd").eq(1).type(userdata.email);
-
-        // Submit the form
-        cy.get("span.NPEfkd").eq(1).click();
         cy.get("span.NPEfkd").eq(1).click();
         cy.wait(1000)
-        
-        // Wait for the confirmation page
+        cy.get("span.NPEfkd").eq(1).click();
         cy.url().should("include", "formResponse");
-        
         cy.wait(1000)
-        // Click the "Submit another response" link
         cy.get('a[href*="viewform"]').click();
       });
     });
